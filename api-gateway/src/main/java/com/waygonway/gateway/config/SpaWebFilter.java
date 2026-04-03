d713f95f02cab51b3108494edb053ea41c532d34
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2026-04-03 11:46:51 +0530

    Fixing SPA root routing in API Gateway

--- api-gateway/src/main/java/com/waygonway/gateway/config/SpaWebFilter.java
+++ api-gateway/src/main/java/com/waygonway/gateway/config/SpaWebFilter.java
@@ -1,17 +1,17 @@
 package com.waygonway.gateway.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-@Configuration
+@Component
 public class SpaWebFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
-        if (!path.startsWith("/api/v1/") && !path.contains(".") && !path.equals("/")) {
+        if (!path.startsWith("/api/v1/") && !path.contains(".") && !path.startsWith("/actuator")) {
             return chain.filter(
                 exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build()).build()
             );
